5140aeec054bf0315ea922337b7eae093d28d035
Dev <Dev597704@gmail.com>
2021-01-02 21:58:30 -0500

    splash page completed, blank instruction and game screen page added.

--- app/src/main/java/com/example/mariotictactoe/MainActivity.java
+++ app/src/main/java/com/example/mariotictactoe/MainActivity.java
@@ -1,14 +1,31 @@
+//Name: Dev Patel
+//Date: April 14, 2020
+
 package com.example.mariotictactoe;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void toins(View view) {
+        Intent i = new Intent(this, Instructions.class);
+        startActivity(i);
+
+    }
+    public void togame(View view) {
+        Intent g = new Intent(this, GameScreen.class);
+        startActivity(g);
+
     }
 }
